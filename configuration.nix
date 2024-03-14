52768c86bd1e730f911ec1c88fa38c900c1665cb
Unknown15082 <trangiahuy15082006@gmail.com>
2024-03-14 16:43:53 +0700

    Initial config, with Plasma 6

---
+++ configuration.nix
@@ -0,0 +1,5 @@
+{
+	imports = [
+		./hosts/fafnir/configuration.nix
+	];
+}
---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+	description = "NixOS configuration flake";
+
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+	};
+
+	outputs = { self, nixpkgs, ... }@inputs : {
+		nixosConfigurations.fafnir = nixpkgs.lib.nixosSystem {
+			system = "x86_64-linux";
+			modules = [
+				./hosts/fafnir/configuration.nix
+			];
+		};
+	};
+}
