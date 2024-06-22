5df5e16e6c59c7217b4a74c2516583f79f3c4bb0
Unknown15082 <trangiahuy15082006@gmail.com>
2024-06-22 23:36:56 +0700

    Added Gamemode

--- hosts/fafnir/configuration.nix
+++ hosts/fafnir/configuration.nix
@@ -117,6 +117,9 @@
 		neovim
 	];
 
+	# Enable Gamemode
+	programs.gamemode.enable = true;
+
 	# Some programs need SUID wrappers, can be configured further or are
 	# started in user sessions.
 	# programs.mtr.enable = true;
