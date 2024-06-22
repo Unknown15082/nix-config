02d0f70c2b08b66097ab238fa18162a19c80cf0c
Unknown15082 <trangiahuy15082006@gmail.com>
2024-06-22 13:22:52 +0700

    Added 8GB swapfile

--- hosts/fafnir/hardware-configuration.nix
+++ hosts/fafnir/hardware-configuration.nix
@@ -25,7 +25,10 @@
 			options = [ "fmask=0022" "dmask=0022" ];
 		};
 
-	swapDevices = [ ];
+	swapDevices = [ {
+		device = "/swapfile";
+		size = 8*1024; # 8GB
+	} ];
 
 	# Enables DHCP on each ethernet and wireless interface. In case of scripted networking
 	# (the default) this is the recommended approach. When using systemd-networkd it's
