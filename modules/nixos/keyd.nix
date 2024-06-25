83cfd51a3ddc32a7dc963f94ad4904a2fe13bd96
Unknown15082 <trangiahuy15082006@gmail.com>
2024-06-25 20:31:47 +0700

    Remove oneshot configs from keyd

--- modules/nixos/keyd.nix
+++ modules/nixos/keyd.nix
@@ -7,13 +7,6 @@
 				main = {
 					# Change CapsLock key to Control/Escape (Hold-Tap)
 					capslock = "overload(control, esc)";
-
-					# Make the modifier keys oneshot
-					shift = "oneshot(shift)";
-					meta = "oneshot(meta)";
-					control = "oneshot(control)";
-					
-					alt = "oneshot(alt)";
 				};
 			};
 		};
