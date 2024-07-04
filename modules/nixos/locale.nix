344bd3eb098b231c4ead71b4ed2ee32310322321
Unknown15082 <trangiahuy15082006@gmail.com>
2024-07-04 13:15:59 +0700

    Move to fcitx5

--- modules/nixos/locale.nix
+++ modules/nixos/locale.nix
@@ -7,10 +7,23 @@
 	};
 
 	# Enable Vietnamese input method
-	/*i18n.inputMethod = {
-		enabled = "ibus";
-		ibus.engines = with pkgs.ibus-engines; [ bamboo ];
-	};*/
+	i18n.inputMethod = {
+		enabled = "fcitx5";
+		fcitx5 = {
+			addons = [ pkgs.fcitx5-bamboo ];
+			waylandFrontend = true;
+
+			settings.inputMethod = {
+				"Groups/0" = {
+					"Name" = "Default";
+					"Default Layout" = "us-altgr-intl";
+					"DefaultIM" = "keyboard-us-altgr-intl";
+				};
+				"Groups/0/Items/0" = { "Name" = "keyboard-us-altgr-intl"; };
+				"Groups/0/Items/1" = { "Name" = "bamboo"; };
+			};
+		};
+	};
 
 	# Automatically setup timezone
 	services.automatic-timezoned.enable = true;
