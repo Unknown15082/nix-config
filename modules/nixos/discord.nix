e8e2c86928ef146df8de94c55a8cf3b172ca30fd
Unknown15082 <trangiahuy15082006@gmail.com>
2024-07-07 20:58:38 +0700

    Discord

--- modules/nixos/discord.nix
+++ modules/nixos/discord.nix
@@ -2,6 +2,11 @@
 {
 	environment.systemPackages = [
 		pkgs.vesktop
+		(pkgs.discord.override {
+			withOpenASAR = true;
+			withVencord = true;
+			withTTS = true;
+		})
 	];
 
 	# Add overrides, which includes OpenASAR, Vencord, TTS, ...
