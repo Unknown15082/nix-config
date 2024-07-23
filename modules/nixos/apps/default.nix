0173f017199aaaefd5b98e64545e980effd06af3
Unknown15082 <trangiahuy15082006@gmail.com>
2024-07-23 15:09:16 +0700

    Docker

--- modules/nixos/apps/default.nix
+++ modules/nixos/apps/default.nix
@@ -3,10 +3,13 @@
 	imports = [
 		./steam.nix
 		./discord.nix
+		./docker.nix
 	];
 
 	modules.steam.enable = lib.mkDefault true;
 
 	modules.discord.enable = lib.mkDefault true;
 	modules.discord.addons = lib.mkDefault true;
+
+	modules.docker.enable = lib.mkDefault true;
 }
---
+++ modules/nixos/apps/docker.nix
@@ -0,0 +1,16 @@
+{ lib, config, ... }:
+let
+	cfg = config.modules.docker;
+in
+{
+	options.modules.docker = {
+		enable = lib.mkEnableOption "docker";
+	};
+
+	config = lib.mkIf cfg.enable {
+		virtualisation.docker = {
+			enable = true;
+			storageDriver = "btrfs";
+		};
+	};
+}
