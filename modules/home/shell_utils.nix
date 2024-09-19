e3c457dad19dabdf628c5e5ee689e51a7c3b1e0b
Unknown15082 <trangiahuy15082006@gmail.com>
2024-09-19 12:28:18 +0800

    Test: pure.fish prompt

--- modules/home/shell_utils.nix
+++ modules/home/shell_utils.nix
@@ -13,12 +13,20 @@ in
 	];
 
 	config = lib.mkIf cfg.enable {
+		home.packages = [ pkgs.grc ];
+
 		programs = {
 			fish = {
 				enable = true;
 				interactiveShellInit = ''
 					set fish_gretting
+					_pure_prompt_new_line
 				'';
+
+				plugins = with pkgs.fishPlugins; [
+					{ name = "grc"; src = grc.src; }
+					{ name = "pure"; src = pure.src; }
+				];
 			};
 
 			eza = {
@@ -40,7 +48,7 @@ in
 			};
 
 			starship = {
-				enable = true;
+				enable = false;
 				enableFishIntegration = true;
 				settings = {
 					add_newline = true;
