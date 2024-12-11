ae85e26baeb93d299f18994851a36d3f5b0ecfd1
Unknown15082 <trangiahuy15082006@gmail.com>
2024-12-11 17:54:53 +0700

    Edit some structure

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -13,9 +13,24 @@ in
 
 		wayland.windowManager.hyprland = {
 			enable = true;
-			extraConfig = ''
-				${builtins.readFile ../../configs/hypr/hyprland.conf}
-			'';
+			extraConfig = builtins.readFile ../../configs/hypr/hyprland.conf;
+
+			settings = {
+				env = [
+					"NIXOS_OZONE_WL,1"
+					"MOZ_ENABLE_WAYLAND,1"
+					"MOZ_WEBRENDER,1"
+				];
+			};
+
+			systemd = {
+				enable = true;
+				variables = [ "--all" ];
+			};
+		};
+
+		xdg.configFile."hypr/original.conf" = {
+			source = ../../configs/hypr/original.conf;
 		};
 	};
 }
