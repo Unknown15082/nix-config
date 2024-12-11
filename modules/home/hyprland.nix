af03d11a776ef4a539bbbdf48721a3e0e1254d7b
Unknown15082 <trangiahuy15082006@gmail.com>
2024-12-11 22:22:27 +0700

    Add more env vars

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -21,7 +21,27 @@ in
 					"NIXOS_OZONE_WL,1"
 					"MOZ_ENABLE_WAYLAND,1"
 					"MOZ_WEBRENDER,1"
-				];
+
+					"GDK_BACKEND,wayland,x11,*"
+					"QT_QPA_PLATFORM,wayland;xcb"
+					"SDL_VIDEODRIVER,wayland"
+					"CLUTTER_BACKEND,wayland"
+
+					"XDG_CURRENT_DESKTOP,Hyprland"
+					"XDG_SESSION_TYPE,wayland"
+					"XDG_SESSION_DESKTOP,Hyprland"
+
+					"QT_AUTO_SCREEN_SCALE_FACTOR,1"
+					"QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
+				] ++ (
+					lib.optionals 
+						config.modules.nvidia.enable
+						[
+							"GBM_BACKEND,nvidia-drm"
+							"__GLX_VENDOR_LIBRARY_NAME,nvidia"
+							"LIBVA_DRIVER_NAME,nvidia"
+						]
+				);
 			};
 
 			systemd = {
