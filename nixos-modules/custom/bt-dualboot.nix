d4a474d889338c136173d6016f5b779c2fd85f39
Unknown15082 <trangiahuy15082006@gmail.com>
2024-12-13 16:05:46 +0700

    Bluetooth Dualboot

---
+++ nixos-modules/custom/bt-dualboot.nix
@@ -0,0 +1,32 @@
+{ pkgs, lib, config, ... }:
+with pkgs.python3Packages;
+let
+	bt-dualboot = buildPythonApplication rec {
+		pname = "bt-dualboot";
+		version = "1.0.1";
+		pyproject = true;
+
+		src = pkgs.fetchPypi {
+			inherit pname version;
+			sha256 = "sha256-pjzGvLkotQllzyrnxqDIjGlpBOvUPkWpv0eooCUrgv8=";
+		};
+
+		dependencies = [
+			poetry-core
+		];
+
+		meta = with lib; {
+			homepage = "https://pypi.org/project/bt-dualboot/";
+			description = "Sync Bluetooth for dualboot Linux and Windows";
+			license = licenses.mit;
+		};
+	};
+in
+{
+	config = lib.mkIf config.modules.bluetooth.enable {
+		environment.systemPackages = with pkgs; [
+			chntpw
+			bt-dualboot
+		];
+	};
+}
