00e5219b5a4bcc6c37e5ac7d454a7600d6b126bb
Unknown15082 <trangiahuy15082006@gmail.com>
2024-12-13 18:11:56 +0700

    Add gnome-keyring on SDDM login

--- nixos-modules/core/sddm.nix
+++ nixos-modules/core/sddm.nix
@@ -8,17 +8,16 @@ in
 		enableKeyring = lib.mkEnableOption "keyring" // { default = cfg.enable; };
 	};
 
-	config = lib.mkIf cfg.enable {
+	config = lib.attrsets.recursiveUpdate (lib.mkIf cfg.enable {
 		services.displayManager = {
 			sddm = {
 				enable = true;
 				wayland.enable = true;
 			};
 		};
-	};
-	# // (lib.mkIf cfg.enableKeyring {
-	# 	services.gnome.gnome-keyring.enable = true;
-	# 	security.pam.services.sddm.enableGnomeKeyring = true;
-	# 	programs.seahorse.enable = true;
-	# });
+	}) (lib.mkIf cfg.enableKeyring {
+		services.gnome.gnome-keyring.enable = true;
+		security.pam.services.sddm.enableGnomeKeyring = true;
+		programs.seahorse.enable = true;
+	});
 }
