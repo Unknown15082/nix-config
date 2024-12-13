1ce0c57fd365ca17fbc7f5c9b84970feeae286cb
Unknown15082 <trangiahuy15082006@gmail.com>
2024-12-13 17:49:44 +0700

    Add SDDM

---
+++ nixos-modules/core/sddm.nix
@@ -0,0 +1,24 @@
+{ lib, config, ... }:
+let
+	cfg = config.modules.sddm;
+in
+{
+	options.modules.sddm = {
+		enable = lib.mkEnableOption "SDDM" // { default = true; };
+		enableKeyring = lib.mkEnableOption "keyring" // { default = cfg.enable; };
+	};
+
+	config = lib.mkIf cfg.enable {
+		services.displayManager = {
+			sddm = {
+				enable = true;
+				wayland.enable = true;
+			};
+		};
+	};
+	# // (lib.mkIf cfg.enableKeyring {
+	# 	services.gnome.gnome-keyring.enable = true;
+	# 	security.pam.services.sddm.enableGnomeKeyring = true;
+	# 	programs.seahorse.enable = true;
+	# });
+}
