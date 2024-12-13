0962308cc6026ec381a5242cb600e5a38e328945
Unknown15082 <trangiahuy15082006@gmail.com>
2024-12-13 18:28:27 +0700

    Change SDDM to GDM

--- nixos-modules/core/gdm.nix
+++ nixos-modules/core/gdm.nix
@@ -1,23 +1,24 @@
 { lib, config, ... }:
 let
-	cfg = config.modules.sddm;
+	cfg = config.modules.gdm;
 in
 {
-	options.modules.sddm = {
-		enable = lib.mkEnableOption "SDDM";
+	options.modules.gdm = {
+		enable = lib.mkEnableOption "GDM" // { default = true; };
 		enableKeyring = lib.mkEnableOption "keyring" // { default = cfg.enable; };
 	};
 
 	config = lib.attrsets.recursiveUpdate (lib.mkIf cfg.enable {
-		services.displayManager = {
-			sddm = {
+		services.xserver = {
+			enable = true;
+			displayManager.gdm = {
 				enable = true;
-				wayland.enable = true;
+				wayland = true;
 			};
 		};
 	}) (lib.mkIf cfg.enableKeyring {
 		services.gnome.gnome-keyring.enable = true;
-		security.pam.services.sddm.enableGnomeKeyring = true;
+		security.pam.services.gdm-password.enableGnomeKeyring = true;
 		programs.seahorse.enable = true;
 	});
 }
