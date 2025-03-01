2a36940b3092d2bfc4cc5241a3e28d6b5f16c7c4
Unknown15082 <trangiahuy15082006@gmail.com>
2025-03-02 00:21:16 +0800

    Ghostty settings

--- home-modules/core/shells/ghostty.nix
+++ home-modules/core/shells/ghostty.nix
@@ -1,6 +1,7 @@
 { lib, config, ... }:
 let
 	cfg = config.modules.ghostty;
+	stylixCfg = config.stylix;
 in
 {
 	options.modules.ghostty = {
@@ -14,7 +15,30 @@ in
 			installBatSyntax = true;
 
 			settings = {
-				# Add settings
+				# Disable emoji fonts
+				font-family = lib.mkForce [
+					stylixCfg.fonts.monospace.name
+				];
+
+				# Disable ligatures
+				font-feature = [ "-calt" "-liga" "-dlig" ];
+
+				# Disable cursor blink
+				cursor-style = "bar";
+				cursor-style-blink = false;
+
+				# Disable mouse while typing
+				mouse-hide-while-typing = true;
+
+				# Disable the resize overlay
+				resize-overlay = "never";
+
+				# Allow clipboard usage
+				clipboard-read = "allow";
+				clipboard-write = "allow";
+
+				# Shell integrations
+				shell-integration-features = "no-cursor,sudo,title";
 			};
 		};
 	};
