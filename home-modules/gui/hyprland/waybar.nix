45d8add10e8e7c82e9db1580aa9085e500198716
Unknown15082 <trangiahuy15082006@gmail.com>
2025-08-28 13:22:18 +0800

    update waybar

--- home-modules/gui/hyprland/waybar.nix
+++ home-modules/gui/hyprland/waybar.nix
@@ -12,6 +12,8 @@ in {
 			systemd.enable = true;
 			
 			settings.mainBar = {
+				position = "left";
+
 				modules-left = [
 					"hyprland/workspaces"
 				];
@@ -22,6 +24,23 @@ in {
 					"battery"
 					"tray"
 				];
+
+				"clock" = {
+					format = "{:%H%n%M}";
+					tooltip-format = "<tt>{calendar}</tt>";
+					locale = "en_GB.UTF-8";
+					calendar = {
+						mode = "year";
+						mode-mon-col = 4;
+						format = {
+							"months" = "<span color='#ffead3'><b>{}</b></span>";
+							"days" = "<span color='#ecc6d9'><b>{}</b></span>";
+							"weeks" = "<span color='#99ffdd'><b>W{}</b></span>";
+							"weekdays" = "<span color='#ffcc66'><b>{}</b></span>";
+							"today" = "<span color='#ff6699'><b><u>{}</u></b></span>";
+						};
+					};
+				};
 			};
 		};
 	};
--- hosts/fafnir/configuration.nix
+++ hosts/fafnir/configuration.nix
@@ -146,4 +146,10 @@
 
 	# Enable I2C
 	hardware.i2c.enable = true;
+
+	# Set locale
+	i18n.extraLocales = [ "en_GB.UTF-8/UTF-8" ];
+	i18n.extraLocaleSettings = {
+		LC_TIME = "en_GB.UTF-8";
+	};
 }
