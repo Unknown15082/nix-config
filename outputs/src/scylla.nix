420f3bd6033d590d94a1e3c1e4a030c328f3f74e
Unknown15082 <trangiahuy15082006@gmail.com>
2026-02-21 22:11:37 +0700

    feat: add deploy node for scylla

--- outputs/src/scylla.nix
+++ outputs/src/scylla.nix
@@ -1,5 +1,7 @@
 {
     libutils,
+    inputs,
+    system,
     ...
 }@args:
 let
@@ -11,11 +13,20 @@ let
         "hosts/${name}/configuration.nix"
     ];
 in
-{
+rec {
     nixosConfigurations = libutils.nixosSystem (
         args
         // {
             inherit nixos-modules;
         }
     );
+
+    nodes = {
+        hostname = name;
+        sshUser = "root";
+        profiles.system = {
+            user = "root";
+            path = inputs.deploy-rs.lib.${system}.activate.nixos nixosConfigurations;
+        };
+    };
 }
