5c08955a7d7a91afa2f84fa7567d241c86335f62
Unknown15082 <trangiahuy15082006@gmail.com>
2026-02-21 16:38:28 +0700

    fix: user description

--- nixos-modules/shared/users.nix
+++ nixos-modules/shared/users.nix
@@ -27,7 +27,7 @@ in
         users.users.${cfg.username} = {
             isNormalUser = normalUser;
             isSystemUser = !normalUser;
-            description = cfg.username;
+            description = lib.mkIf normalUser cfg.username;
             extraGroups = cfg.extraGroups ++ [ "wheel" ];
             # TODO: hashPasswordFile
             # TODO: authorizedKeys
