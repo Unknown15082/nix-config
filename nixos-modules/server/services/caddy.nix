66c3553798f979772e94185a13787776f8fb504f
Unknown15082 <trangiahuy15082006@gmail.com>
2026-03-11 18:44:44 +0800

    fix(caddy): fix hash

--- nixos-modules/server/services/caddy.nix
+++ nixos-modules/server/services/caddy.nix
@@ -25,7 +25,7 @@ in
                     "github.com/greenpau/caddy-security@v1.1.31"
                     "github.com/WeidiDeng/caddy-cloudflare-ip@v0.0.0-20231130002422-f53b62aa13cb"
                 ];
-                hash = "sha256-9ZcyVfnpIIBUrwkjZHuTROSqVjUafXa7B5zFuGA3QMc=";
+                hash = "sha256-93FZF2mrJPRWIcXeA2Ja6oWkg20uflCAE2R6cwCMPSk=";
             };
 
             environmentFile = config.age.secrets.caddy_env.path;
