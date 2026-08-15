f09593caa3e396eb4e36027537816bc1a527de2d
Unknown15082 <trangiahuy15082006@gmail.com>
2026-08-15 16:53:35 +0000

    feat!: hardware aspect per-host

--- modules/hosts/bahamut.nix
+++ modules/hosts/bahamut.nix
@@ -1,5 +1,46 @@
 { self, inputs, ... }: {
-	flake.modules.nixos.hosts.bahamut = { pkgs, ... }: {
+	flake.modules.nixos.hosts.bahamut = { config, ... }: {
+		imports = with config.flake.modules.nixos; [
+			bahamutHardware
+		];
+	};
+
+	flake.modules.nixos.hosts.bahamutHardware = { config, ... }: {
+		boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usbhid" "usb_storage" "sd_mod" ];
+		boot.initrd.kernelModules = [];
+		boot.kernelModules = [ "kvm-intel" ];
+		boot.extraModulePackages = [];
+
+		nixpkgs.hostPlatform = "x86_64-linux";
+		hardware.enableRedistributableFirmware = true;
+		hardware.cpu.intel.npu.enable = true;
+		hardware.cpu.intel.updateMicrocode = config.hardware.enableRedistributableFirmware;
+
+		fileSystems."/" = {
+			device = "/dev/disk/by-label/NIXOS";
+			fsType = "btrfs";
+			options = [ "subvol=root" "compress=zstd" "space_cache=v2" "ssd" ];
+		};
+
+		fileSystems."/home" = {
+			device = "/dev/disk/by-label/NIXOS";
+			fsType = "btrfs";
+			options = [ "subvol=home" "compress=zstd" "space_cache=v2" "ssd" ];
+		};
+
+		fileSystems."/nix" = {
+			device = "/dev/disk/by-label/NIXOS";
+			fsType = "btrfs";
+			options = [ "subvol=home" "compress=zstd" "noatime" "space_cache=v2" "ssd" ];
+		};
+
+		fileSystems."/boot" = {
+			device = "/dev/disk/by-label/NIXBOOT";
+			fsType = "vfat";
+			options = [ "fmask=0022" "dmask=0022" ];
+		};
+
+		swapDevices = [];
 	};
 
 	flake.nixosConfigurations.bahamut = inputs.nixpkgs.lib.nixosSystem {
