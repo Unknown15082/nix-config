775b112c47fcec5eca1ab326e6b80c3b6d9b4ea2
Unknown15082 <trangiahuy15082006@gmail.com>
2026-08-15 15:45:43 +0000

    feat!: new bahamut host

---
+++ hosts/bahamut/configuration.nix
@@ -0,0 +1,24 @@
+{ pkgs, lib, myvars, ... }:
+{
+	imports = [
+		./hardware-configuration.nix
+	];
+
+	networking.hostName = "bahamut";
+
+	presets.desktop.enable = true;
+	modules.nvidia.enable = lib.mkForce false;
+
+	modules.users.username = myvars.username;
+
+	programs.fish.enable = true;
+	users.defaultUserShell = pkgs.fish;
+
+	modules.hyprland.enable = true;
+
+	modules.bluetooth.bt-sync.enable = false;
+
+	services.openssh.enable = true;
+
+	system.stateVersion = "26.05";
+}
