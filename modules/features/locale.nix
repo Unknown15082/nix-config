222ad60a25258da5f8160c295bda4a6087537b1b
Unknown15082 <trangiahuy15082006@gmail.com>
2026-08-16 08:25:06 +0000

    fix(locale): change some wrong parameters

--- modules/features/locale.nix
+++ modules/features/locale.nix
@@ -1,4 +1,4 @@
-{ self, pkgs, ... }: {
+{ self, ... }: {
 	flake.modules.nixos.locale = {
 		i18n.extraLocales = [ "en_GB.UTF-8/UTF-8" ];
 		i18n.extraLocaleSettings = {
@@ -10,14 +10,14 @@
 		imports = with self.modules.nixos; [ codeFonts cjkFonts ];
 	};
 
-	flake.modules.nixos.codeFonts = {
+	flake.modules.nixos.codeFonts = { pkgs, ... }: {
 		fonts.packages = with pkgs; [
 			jetbrains-mono
 			nerd-fonts.jetbrains-mono
 		];
 	};
 
-	flake.modules.nixos.cjkFonts = {
+	flake.modules.nixos.cjkFonts = { pkgs, ... }: {
 		fonts.packages = with pkgs; [
 			dejavu_fonts
 			noto-fonts-cjk-sans
